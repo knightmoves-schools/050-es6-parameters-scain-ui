966531ae427d688d71b51f51f13a5b4ee1568d77
scain-ui <scain@midland.k12.ia.us>
2025-11-17 18:18:44 +0000

    2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function calculateTotal(subtotal, shipping = 2.50) {
   return subtotal + shipping;
-}
+};
 
 function printTopThreeHeadlines(...headlines) {
   return headlines.slice(0, 3).join('\n');
-}
+};
 
 export { calculateTotal, printTopThreeHeadlines };
